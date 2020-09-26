490e91fd258ecd662c347eebfacc35d54792c64d
Christopher Murray <cmurray1105@gmail.com>
2020-09-26 14:40:45 -0500

    commit

---
+++ database/database.js
@@ -0,0 +1,64 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const config = require('./config.js')
+// const userRoutes = require('../routes/user.js');
+// const assert = require('assert');
+// const app = express();
+
+
+// const userColl = config.userCollName
+
+const mongoose = require("mongoose");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const CONFIG = require('./config.js');
+const { ObjectID } = require('mongodb').ObjectID;
+const uri = config.uri;
+const dbName = config.dbName;
+const itemCollName = CONFIG.itemCollName;
+const userCollName = CONFIG.userCollName;
+const achievementCollName = CONFIG.achievementCollName;
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+
+
+mongoose.connect(
+  `mongodb+srv://FarmdDev:${config.password}@farm2market.lkakx.mongodb.net/${config.dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
+).then(()=>{
+  console.log("connected to MONGO!!")
+})
+
+mongoose.Promise = global.Promise;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/user", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
