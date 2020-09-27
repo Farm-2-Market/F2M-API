c654f2d8a101b3e49f7894468bcbd9cf45633098
Christopher Murray <cmurray1105@gmail.com>
2020-09-27 14:23:13 -0500

    commit'

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const http = require('http')
-const app = require('./database/database')
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
 // const server = http.createServer(app);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
