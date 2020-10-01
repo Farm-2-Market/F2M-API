05fa370535cde98ea14b296282f2471e58c1e942
Christopher Murray <cmurray1105@gmail.com>
2020-10-01 16:25:23 -0500

    commit

--- server.compiled.js
+++ server.compiled.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const InitiateMongoServer = require('./database/database');
+// const InitiateMongoServer = require('./database/database');
 const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-InitiateMongoServer();
+// InitiateMongoServer();
 
 app.use(bodyParser.json());
 // const server = http.createServer(app);
