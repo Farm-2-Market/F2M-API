9f3659bbb7e93d24cf3566a71f00ff38c1326c34
Christopher Murray <cmurray1105@gmail.com>
2020-10-01 16:19:30 -0500

    commit

--- database/database.js
+++ database/database.js
@@ -1,80 +1,80 @@
-const mongoose = require("mongoose");
-const MongoClient = require('mongodb').MongoClient
-const config = require('./config');
-
-const InitiateMongoServer = async () => {
-  console.log(config)
-  try {
-    await mongoose.connect(config.uri, {useNewUrlParser: true, useUnifiedTopology: true
-    });
-    console.log("woohoo!");
-  } catch (e) {
-    console.log(e)
-    throw e
-  }
-  };
-
-  module.exports = InitiateMongoServer;
-
-// const express = require("express");
-// // const mongoose = require("mongoose");
-// // const config = require('./config.js')
-// const userRoutes = require('../routes/user.js');
-// // const assert = require('assert');
-// const app = express();
-
-
-// const userColl = config.userCollName
-
 // const mongoose = require("mongoose");
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
+// const MongoClient = require('mongodb').MongoClient
+// const config = require('./config');
 
-// const { ObjectID } = require('mongodb').ObjectID;
-// const uri = config.uri;
-// const dbName = config.dbName;
-
-
-// const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-
-
-// mongoose.connect(
-//   `mongodb+srv://FarmdDev:${config.password}@farm2market.lkakx.mongodb.net/${config.dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
-// ).then(()=>{
-//   console.log("connected to MONGO!!")
-// })
-
-// mongoose.Promise = global.Promise;
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//     return res.status(200).json({});
+// const InitiateMongoServer = async () => {
+//   console.log(config)
+//   try {
+//     await mongoose.connect(config.uri, {useNewUrlParser: true, useUnifiedTopology: true
+//     });
+//     console.log("woohoo!");
+//   } catch (e) {
+//     console.log(e)
+//     throw e
 //   }
-//   next();
-// });
+//   };
 
-// // Routes which should handle requests
-// app.use("/user", userRoutes);
+//   module.exports = InitiateMongoServer;
 
-// app.use((req, res, next) => {
-//   const error = new Error("Not found");
-//   error.status = 404;
-//   next(error);
-// });
+// // const express = require("express");
+// // // const mongoose = require("mongoose");
+// // // const config = require('./config.js')
+// // const userRoutes = require('../routes/user.js');
+// // // const assert = require('assert');
+// // const app = express();
 
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.json({
-//     error: {
-//       'ln 57 message' : error.message
-//     }
-//   });
-// });
 
-// module.exports = app;
+// // const userColl = config.userCollName
+
+// // const mongoose = require("mongoose");
+// // const MongoClient = require('mongodb').MongoClient;
+// // const assert = require('assert');
+
+// // const { ObjectID } = require('mongodb').ObjectID;
+// // const uri = config.uri;
+// // const dbName = config.dbName;
+
+
+// // const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+
+
+// // mongoose.connect(
+// //   `mongodb+srv://FarmdDev:${config.password}@farm2market.lkakx.mongodb.net/${config.dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }
+// // ).then(()=>{
+// //   console.log("connected to MONGO!!")
+// // })
+
+// // mongoose.Promise = global.Promise;
+// // app.use((req, res, next) => {
+// //   res.header("Access-Control-Allow-Origin", "*");
+// //   res.header(
+// //     "Access-Control-Allow-Headers",
+// //     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+// //   );
+// //   if (req.method === "OPTIONS") {
+// //     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+// //     return res.status(200).json({});
+// //   }
+// //   next();
+// // });
+
+// // // Routes which should handle requests
+// // app.use("/user", userRoutes);
+
+// // app.use((req, res, next) => {
+// //   const error = new Error("Not found");
+// //   error.status = 404;
+// //   next(error);
+// // });
+
+// // app.use((error, req, res, next) => {
+// //   res.status(error.status || 500);
+// //   res.json({
+// //     error: {
+// //       'ln 57 message' : error.message
+// //     }
+// //   });
+// // });
+
+// // module.exports = app;
