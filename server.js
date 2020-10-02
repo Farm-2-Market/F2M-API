df04a8b61a9c5b06b78a27cc468efd066611b561
Christopher Murray <cmurray1105@gmail.com>
2020-10-02 18:17:54 -0500

    new mongo hash

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db()
   //user does not exist yet
   try {
     let newUser= new User({
-      _id: Mongoose.Schema.Types.ObjectId(),
+      _id: new ObjectID(),
       email: `${req.body.email}`,
       username: `${req.body.username}`,
       password: `${req.body.password}`
