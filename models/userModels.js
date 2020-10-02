a29389cbd9b3e00bb0ca9883a2eb95cd63162466
Christopher Murray <cmurray1105@gmail.com>
2020-10-02 17:54:01 -0500

    test2

--- models/userModels.js
+++ models/userModels.js
@@ -1,15 +1,54 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        // match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-    username: { type: String, required: true },
+// const userSchema = mongoose.Schema({
+//     _id: mongoose.Schema.Types.ObjectId,
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         // match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+//     },
+//     username: { type: String, required: true },
+//     password: { type: String, required: true }
+// });
+const mongoose = require('mongoose&#8217'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.pre(save, function(next) {
+    var user = this;
+
+// only hash the password if it has been modified (or is new)
+if (!user.isModified('password')) return next();
+
+// generate a salt
+bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+    });
+});
+
+
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model(User, UserSchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bcrypt = require('bcrypt');
 const app = express();
 const Mongoose=require('mongoose');
+const Schema = Mongoose.Schema
 // const UserModel = require('./models/userModels');
 const port = process.env.PORT || 3000;
 const db = require('./database/database');
@@ -11,18 +12,7 @@ const bodyParser=require('body-parser');
   app.use(bodyParser.urlencoded({
   extended: true
 }));
-
-const UserModel = new Mongoose.model("user", {
-  _id: Mongoose.Schema.Types.ObjectId,
-  email: {
-      type: String,
-      required: true,
-      unique: true
-  },
-  username: { type: String, required: true },
-  password: { type: String, required: true }
-});
-// InitiateMongoServer();
+const User = require('./models/userModels');
 
 app.use(bodyParser.json());
 // const server = http.createServer(app);
@@ -30,11 +20,41 @@ app.post("/signup", async (request, response) => {
   db();
   try {
     // console.log('test', request);
-      request.body.password = bcrypt.hashSync(request.body.password, 10);
-      console.log(request.body.password)
-      var user = new UserModel(request.body);
-      var result = await user.save();
-      response.send(result);
+      // request.body.password = bcrypt.hashSync(request.body.password, 10);
+
+      let newUser= new User({
+        email: `${email}`,
+        username: `${username}`,
+        password: `${password}`
+      })
+      newUser.save((err)=>{
+        if (err){
+          throw err;
+          User.findOne({ username:`${username}`,  function(err, user) {
+            if (err) throw err;
+
+    // test a matching password
+    user.comparePassword('Password123', function(err, isMatch) {
+      if (err) throw err;
+      console.log('Password123:', isMatch); // -> Password123: true
+  });
+
+  // test a failing password
+  user.comparePassword('123Password', function(err, isMatch) {
+      if (err) throw err;
+      console.log('123Password:', isMatch); // -> 123Password: false
+  });
+}
+          })
+        }
+      }
+
+
+
+      // console.log(request.body.password)
+
+      // var result = await user.save();
+      // response.send(result);
   } catch (error) {
       response.status(500).send(error);
   }
