3d99ff92902cf8a81579133642a8eee563914d33
Christopher Murray <cmurray1105@gmail.com>
2020-10-02 12:20:36 -0500

    changes

--- database/config.js
+++ database/config.js
@@ -1,7 +1,8 @@
-// const config = {
-//   uri: "mongodb+srv://FarmdDev:Sox2004!@farm2market.lkakx.mongodb.net/F2MDB?retryWrites=true&w=majority",
-//   dbName: 'F2MDB',
-//   userCollName: 'users',
-//   password:'Sox2004!'
-// };
-// module.exports = config;
+const config = {
+  uri: "mongodb+srv://FarmdDev:Sox2004!@farm2market.lkakx.mongodb.net/F2MDB?retryWrites=true&w=majority",
+  dbName: 'F2MDB',
+  userCollName: 'users',
+  password:'Sox2004!'
+}
+
+module.exports = config;
