9003439e57791c5ae62eed4e9c72d3038019c201
Christopher Murray <cmurray1105@gmail.com>
2020-10-03 04:09:02 -0500

    got rid of es6 syntax function declarations

--- models/userModels.js
+++ models/userModels.js
@@ -15,16 +15,16 @@ var UserSchema = new Schema({
     password: { type: String, required: true }
 });
 
-UserSchema.pre('save', (next)=>{
+UserSchema.pre('save', function (next) {
     var user = this;
 
 // only hash the password if it has been modified (or is new)
 // if (!user.isModified('password')) return next();
 // generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt)=>{
+bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt){
     if (err) return next(err);
     // hash the password using our new salt
-    bcrypt.hash(user.password, salt, (err, hash)=> {
+    bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
         // override the cleartext password with the hashed one
         user.password = hash;
@@ -35,7 +35,7 @@ bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt)=>{
 
 });
 
-UserSchema.methods.comparePassword = (candidatePassword, cb) =>{
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, (err, isMatch)=>{
         if (err) return cb(err);
         cb(null, isMatch);
