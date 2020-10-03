d9717f9c5c9cc58ed5c1840b8937fdf3e11cf5bb
Christopher Murray <cmurray1105@gmail.com>
2020-10-03 05:32:00 -0500

    commit

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.use(
     extended: true,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(bodyParser.json());
