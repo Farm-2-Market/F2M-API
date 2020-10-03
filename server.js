75bc0aa6a8809a9d84c77a2dd892e69683d193d5
Christopher Murray <cmurray1105@gmail.com>
2020-10-03 04:48:38 -0500

    commit

--- server.js
+++ server.js
@@ -1,77 +1,69 @@
-const express = require('express');
-const cors = require('cors')
-const bcrypt = require('bcrypt');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const Mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./database/database");
+const User = require("./models/userModels");
+const Schema = Mongoose.Schema;
 const app = express();
-const Mongoose=require('mongoose');
-const Schema = Mongoose.Schema
-// const UserModel = require('./models/userModels');
 const port = process.env.PORT || 3000;
-const db = require('./database/database');
-const bodyParser=require('body-parser');
-  app.use(cors())
-  app.use(bodyParser.urlencoded({
-  extended: true
-}));
-const User = require('./models/userModels');
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 
 app.use(bodyParser.json());
 // const server = http.createServer(app);
 app.post("/signup", async (req, res) => {
   // make db queryies and res.json the data
-db()
+  db();
 
   //user does not exist yet
   try {
-    let newUser= new User({
+    let newUser = new User({
       _id: Mongoose.Types.ObjectId(),
       email: `${req.body.email}`,
       username: `${req.body.username}`,
-      password: `${req.body.password}`
-    })
+      password: `${req.body.password}`,
+    });
 
-      newUser.save((err)=>{
-        if (err){
-          console.log(err);
-        }
-          User.findOne({ username:`${req.body.username}`},   function (err, user) {
-            if (err) console.log(err);
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      }
+      User.findOne({ username: `${req.body.username}` }, function (err, user) {
+        if (err) console.log(err);
 
-    // test a matching password
-    user.comparePassword(`${req.body.password}`, function(err, isMatch) {
-      if (err) console.log(err);
-      console.log('Password123:', isMatch); // -> Password123: true
-  });
+        // test a matching password
+        user.comparePassword(`${req.body.password}`, function (err, isMatch) {
+          if (err) console.log(err);
+          console.log("Password Matches:", isMatch); // -> Password123: true
+        });
+      });
+    });
 
-  // test a failing password
-  user.comparePassword(`${req.body.password}`, function(err, isMatch) {
-      if (err) console.log(err);
-      console.log('123Password:', isMatch); // -> 123Password: false
-  });
-})
+    console.log(request.body.password);
 
-        })
-
-
-      console.log(request.body.password)
-
-      var result = await user.save();
-      res.status(200).send("hello")
+    var result = await user.save();
+    res.status(200).send("hello");
+  } catch (error) {
+    res.status(500).send(error);
   }
- catch (error) {
-      res.status(500).send(error);
-  }
-})
-app.get('/', (req, res) => {
+});
+app.get("/", (req, res) => {
   db();
-  res.send(`Hello Amazon! ${port}`)
-
-})
+  res.send(`Hello Amazon! ${port}`);
+});
 // app.get('/things', (req, res) => {
 //   console.log('server')
 //   res.send(['hello from the farm to market api'])
 // })
 
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
